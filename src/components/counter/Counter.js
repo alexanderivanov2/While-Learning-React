d6cc9a8c251038174b5ffc81d8095591f45369a2
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-09-14 21:20:49 +0300

    counter add and subtract
    made add and subtract functionality to the counter.

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -4,13 +4,21 @@ import './Counter.css';
 function Counter() {
     const [countResult, setCountResult] = useState(0);
 
+    const countResultAddition = (e) => {
+        setCountResult(state => state + 1);
+    }
+
+    const countResultSubtraction = (e) => {
+        setCountResult(state => state - 1 < 0 ? 0 : state - 1);
+    }
+
     return (
         <>
         <section className='counterBox'>
             <h3 className="countResult">{countResult}</h3>
             <article className='controls'>
-                <div className="add control">+</div>
-                <div className="minus control">-</div>
+                <div className="add control" onClick={countResultAddition}>+</div>
+                <div className="subtract control" onClick={countResultSubtraction}>-</div>
             </article>
         </section>
         </>
