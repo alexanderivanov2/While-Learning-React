0ef0cdedcdcb6be135b269618471033d9033753a
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-09-22 19:21:22 +0300

    create Basic Registration Form layout and style

--- src/components/basicRegistrationForm/BasicRegistrationForm.js
+++ src/components/basicRegistrationForm/BasicRegistrationForm.js
@@ -1,7 +1,17 @@
+import styles from './BasicRegistrationForm.module.css'
+
 function BasicRegistrationForm() {
     return (
         <>
-            <h2>Basic Registration Form</h2>  
+            <h2 className={styles.title}>Basic Registration Form</h2>  
+            <section className={styles.basicRegistrationSection}>
+                <form className={styles.basicRegistrationForm}>
+                    <input type="text" placeholder="First Name" className={`${styles.inputStructure} ${styles.inputText}`}/>
+                    <input type="text" placeholder="Last Name" className={`${styles.inputStructure} ${styles.inputText}` }/>
+                    <input type="text" placeholder="Email" className={`${styles.inputStructure} ${styles.inputText}` }/>
+                    <input type="submit" className={`${styles.inputStructure} ${styles.inputSubmit}`} value="Register"/>
+                </form>
+            </section>
         </>
     );
 }
