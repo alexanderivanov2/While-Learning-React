b8d4153efffbf239785169e752553ffe5b4cff76
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-10 23:59:41 +0300

    build add functionality

--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -9,6 +9,7 @@ function TodoList() {
     const [todos, setTodos] = useState([]);
     const [filterTodos, setFilterTodos] = useState('All');
     const [filtredTodos, setFilteredTodos] = useState([]);
+    const [todoInput, setTodoInput] = useState('');
     const [isChange, setIsChange] = useState(true);
 
     useEffect(() => {
@@ -20,12 +21,25 @@ function TodoList() {
             setIsChange(false);
         }
         
-    }, [filterTodos, isChange]);
+    }, [isChange]);
 
     const onChangeFilterCriteria = (e) => {
         const filterValue = e.target.value;
         setFilterTodos(filterValue);
         setIsChange(true);
+    }
+
+    const addTodo = () => {
+        if (todoInput.length > 2) {
+            setTodos(state => {
+                const newTodos = [...state, {todo: todoInput, isCompleted: false}];
+                TodosSetInLS(newTodos);
+                return newTodos;
+            });
+            setTodoInput('');
+            setIsChange(true);
+        }
+
     }
 
     const completeTodo = (itemData, i) => {
@@ -47,6 +61,11 @@ function TodoList() {
         setIsChange(true);
     }
 
+    const onInputChange = (e) => {
+        console.log(e.target.value);
+        setTodoInput(e.target.value);
+    }
+
     return (
         <>
             <BackButton/>
@@ -54,7 +73,13 @@ function TodoList() {
                 <h2 className={styles.todoListAppPage__title}>Todo List App</h2>
                 <article className={styles.todoListAppPage__app}>
                     <div className={styles.todoListAppPage__app__addAndFilterWrapper}>
-                        <input className={styles.todoListAppPage__app__addAndFilterWrapper__create} id='createTodo'  type='text' placeholder='Todo...'/>
+                        <div className={styles.todoListAppPage__app__addAndFilterWrapperCreate}>
+                            <input className={styles.todoListAppPage__app__addAndFilterWrapperCreate__create} id='createTodo' value={todoInput}
+                            onChange={(e) => onInputChange(e)} type='text' placeholder='Todo...'/>
+                            <button className={styles.todoListAppPage__app__addAndFilterWrapperCreate__add} onClick={addTodo}>+</button>
+
+                        </div>
+                        
                         <select className={styles.todoListAppPage__app__addAndFilterWrapper__filter} id='filterTodo' value={filterTodos.filter} onChange={onChangeFilterCriteria}>
                             <option value='All'>All</option>
                             <option value='Completed'>Completed</option>
