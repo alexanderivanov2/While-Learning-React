4cfad2d5e780f0bededf5a5b1ebd802c30edb5c2
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-17 12:57:20 +0300

    build Number Button component

--- src/components/calculator/NumberBtn.js
+++ src/components/calculator/NumberBtn.js
@@ -2,9 +2,11 @@ import styles from './Calculator.module.scss';
 
 function NumberBtn({
     number,
+    data,
+    onClickBtn
 }) {
     return (
-        <button className={styles.calculatorApp__controls__btn}>{number}</button>
+        <button className={styles.calculatorApp__controls__btn} onClick={(e) => onClickBtn(e, data)}>{number}</button>
     );
 }
 
